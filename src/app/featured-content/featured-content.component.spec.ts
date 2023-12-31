fec7eb8cabc2a3eeabc1afe2d5113e90cd1aa51a
AYGA2K <garouatayoub@gmail.com>
2023-12-31 02:59:55 +0100

    add features section and footer

--- src/app/featured-content/featured-content.component.spec.ts
+++ src/app/featured-content/featured-content.component.spec.ts
@@ -1,14 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FeaturedContentComponent } from './featured-content.component';
-
 describe('FeaturedContentComponent', () => {
   let component: FeaturedContentComponent;
   let fixture: ComponentFixture<FeaturedContentComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FeaturedContentComponent]
+      declarations: [FeaturedContentComponent],
     });
     fixture = TestBed.createComponent(FeaturedContentComponent);
     component = fixture.componentInstance;
